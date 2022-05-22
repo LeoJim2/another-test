098f241939d7b2743bd1ef9d83dad16a4fcef3d9
LeoJim2 <megaizlu@gmail.com>
2022-05-22 09:34:51 -0500

    testing

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require("express");
+import express from "express";
+
+import path from "path";
+import morgan from "morgan";
+import cors from "cors";
+
 const app = express();
 
-const path = require("path");
-const logger = require("morgan");
-const cors = require("cors");
-
-app.use(logger("dev"));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
